b8fd5ac69a9488e20b999d3559de637da017a0d0
John Hite <jhite@renttherunway.com>
2017-09-17 10:08:00 -0400

    making Foo service use RDS

--- barservice/bar-service/start.sh
+++ barservice/bar-service/start.sh
@@ -19,5 +19,6 @@ wait_for_it() {
 
 wait_for_it
 sleep 5
+mkdir /var/log/envoy
 /usr/bin/java -Xbootclasspath/p:/opt/rtr/alpn-boot.jar -jar bar-service.jar server production.yml &
 /usr/local/bin/envoy -l info --service-cluster bar --service-node `hostname` -c envoy.json
--- fooservice/foo-service/start.sh
+++ fooservice/foo-service/start.sh
@@ -19,5 +19,6 @@ wait_for_it() {
 
 wait_for_it
 sleep 5
+mkdir /var/log/envoy
 /usr/bin/java -Xbootclasspath/p:/opt/rtr/alpn-boot.jar -jar foo-service.jar server production.yml &
 /usr/local/bin/envoy -l info --service-cluster foo --service-node `hostname` -c envoy.json
